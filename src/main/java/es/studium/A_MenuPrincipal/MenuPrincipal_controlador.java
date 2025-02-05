593d3c7e9ce9a87997818d45ef98623ed690ccad
JavPueEsc <javierpueyoescalante@gmail.com>
2025-02-05 20:48:32 +0100

    Version 15.0 generación de informes operativa

--- src/main/java/es/studium/A_MenuPrincipal/MenuPrincipal_controlador.java
+++ src/main/java/es/studium/A_MenuPrincipal/MenuPrincipal_controlador.java
@@ -1,7 +1,18 @@
 package es.studium.A_MenuPrincipal;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
 
 import es.studium.B1_AltaArticulo.AltaArticulo_controlador;
 import es.studium.B1_AltaArticulo.AltaArticulo_vista;
@@ -19,12 +30,22 @@ import es.studium.C3_ModificarTicket.ModificarTicket1_controlador;
 import es.studium.C3_ModificarTicket.ModificarTicket1_vista;
 import es.studium.C4_ConsultarTicket.ConsultarTicket_controlador;
 import es.studium.C4_ConsultarTicket.ConsultarTicket_vista;
+import es.studium.Z_Modelos.GestorConexiones;
 import es.studium.Z_Modelos.Modelo;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class MenuPrincipal_controlador implements ActionListener{
 	
 	MenuPrincipal_vista vista;
 	Modelo modelo;
+	LocalDate fechaActual = LocalDate.now();
+	String fechaGeneracion = fechaActual.toString();
 	
 	public MenuPrincipal_controlador(MenuPrincipal_vista v, Modelo m) {
 		vista = v;
@@ -41,8 +62,12 @@ public class MenuPrincipal_controlador implements ActionListener{
 		v.mnuiEliminarTicket.addActionListener(this);
 		v.mnuiModificarTicket.addActionListener(this);
 		v.mnuiConsultarTicket.addActionListener(this);
-		
 		v.mnuiSalir.addActionListener(this);
+		
+		//Añadir Listener a los nuevos menú items
+		v.mniInformeArticulos.addActionListener(this);
+		v.mniInformeTickets.addActionListener(this);
+		v.btnDlgAceptar.addActionListener(this);
 		
 		// Colocar imagen de fondo
         int anchoOriginal = v.imagenFondo.getIconWidth();
@@ -105,9 +130,147 @@ public class MenuPrincipal_controlador implements ActionListener{
 			vista.setVisible(false);
 		}
 		
+		//Gestión de eventos en los nuevos Menú Items "Informe artículo" e "Informe Tickets"
+		
+		//1. Generar Informe sobre Artículos
+		
+		if(e.getSource().equals(vista.mniInformeArticulos)) {
+			try {
+				// Compilar el informe generando fichero .jasper
+					JasperCompileManager.compileReportToFile("./src/main/resources/es/studium/jasper/ArticulosTiendecitajpe.jrxml");
+					
+				// Objeto para guardar parámetros necesarios para el informe
+							HashMap<String, Object> parametros = new HashMap<String, Object>();
+							parametros.put("fecha_generacion", Modelo.mysqlEuropeo(fechaGeneracion));
+				// Cargar el informe compilado
+							JasperReport report = (JasperReport) JRLoader
+									.loadObjectFromFile("./src/main/resources/es/studium/jasper/ArticulosTiendecitajpe.jasper");
+
+				// Conectar a la base de datos para sacar la información
+							Connection conexion = GestorConexiones.getMySQL_Connection("tiendecitajpe");
+							
+				// Completar el informe con los datos de la base de datos
+							JasperPrint print = JasperFillManager.fillReport(report, parametros, conexion);
+
+				// Mostrar el informe en JasperViewer
+							JasperViewer.viewReport(print, false);
+
+				// Para exportarlo a pdf
+							JasperExportManager.exportReportToPdfFile(print,
+									"./src/main/resources/es/studium/jasper/ArticulosTiendecitajpe.pdf");
+
+				//Crea el informe en PDF
+							File path = new File("./src/main/resources/es/studium/jasper/ArticulosTiendecitajpe.pdf");
+
+				// Abrir el fichero PDF generado
+							Desktop.getDesktop().open(path);
+							System.out.println("Fichero ArticulosTiendecitajpe.pdf generado CORRECTAMENTE!");
+
+						} catch (Exception ex1) {
+
+							System.out.println("Error: " + e.toString());
+
+						}
+		}
+		
+		//2. Generar Informe sobre tickets
+		
+		if(e.getSource().equals(vista.mniInformeTickets)) {
+			
+			vista.dlgTickets.setVisible(true);
+					
+		}
+		
+		if(e.getSource().equals(vista.btnDlgAceptar)){
+			boolean fechaDesdeValida = Modelo.esFechaValida(vista.txtFechaDesde.getText());
+			boolean fechaHastaValida = Modelo.esFechaValida(vista.txtFechaHasta.getText());
+			
+			if(vista.txtFechaDesde.getText().isEmpty()) {
+				JOptionPane.showMessageDialog(
+					    vista,
+					    "El campo 'Fecha desde:' no puede estar vacío.",
+					    "Error",
+					    JOptionPane.ERROR_MESSAGE
+					);
+			}
+			
+			else if (vista.txtFechaHasta.getText().isEmpty()) {
+				JOptionPane.showMessageDialog(
+					    vista,
+					    "El campo 'Fecha Hasta:' no puede estar vacío.",
+					    "Error",
+					    JOptionPane.ERROR_MESSAGE
+					);
+			}
+			
+			else if(!fechaDesdeValida) {
+				JOptionPane.showMessageDialog(
+					    vista,
+					    "La fecha del campo 'Fecha Desde:' debe tener el formato dd/mm/aaaa.",
+					    "Error",
+					    JOptionPane.ERROR_MESSAGE
+					);
+			}
+			
+			else if(!fechaHastaValida) {
+				JOptionPane.showMessageDialog(
+					    vista,
+					    "La fecha del campo 'Fecha Hasta:' debe tener el formato dd/mm/aaaa.",
+					    "Error",
+					    JOptionPane.ERROR_MESSAGE
+					);
+			}
+			
+			else {
+				try {
+					// Compilar el informe generando fichero .jasper
+						JasperCompileManager.compileReportToFile("./src/main/resources/es/studium/jasper/TicketsTiendecitajpe.jrxml");
+						
+					// Objeto para guardar parámetros necesarios para el informe
+								HashMap<String, Object> parametros = new HashMap<String, Object>();
+								parametros.put("fecha_generacion", Modelo.mysqlEuropeo(fechaGeneracion));
+								parametros.put("fechaInicio", vista.txtFechaDesde.getText());
+								parametros.put("fechaFin", vista.txtFechaHasta.getText());
+								
+								System.out.println(Modelo.europeoMysql(vista.txtFechaDesde.getText()));
+								System.out.println(Modelo.europeoMysql(vista.txtFechaHasta.getText()));
+					// Cargar el informe compilado
+								JasperReport report = (JasperReport) JRLoader
+										.loadObjectFromFile("./src/main/resources/es/studium/jasper/TicketsTiendecitajpe.jasper");
+
+					// Conectar a la base de datos para sacar la información
+								Connection conexion2 = GestorConexiones.getMySQL_Connection("tiendecitajpe");
+								
+					// Completar el informe con los datos de la base de datos
+								JasperPrint print = JasperFillManager.fillReport(report, parametros, conexion2);
+
+					// Mostrar el informe en JasperViewer
+								JasperViewer.viewReport(print, false);
+
+					// Para exportarlo a pdf
+								JasperExportManager.exportReportToPdfFile(print,
+										"./src/main/resources/es/studium/jasper/TicketsTiendecitajpe.pdf");
+
+					//Crea el informe en PDF
+								File path = new File("./src/main/resources/es/studium/jasper/TicketsTiendecitajpe.pdf");
+
+					// Abrir el fichero PDF generado
+								Desktop.getDesktop().open(path);
+								System.out.println("Fichero TicketsTiendecitajpe.pdf generado CORRECTAMENTE!");
+					// cerrar cuadro de diálogo
+								
+								vista.dlgTickets.setVisible(false);
+
+							} catch (Exception ex2) {
+
+								System.out.println("Error: " + e.toString());
+
+							}
+			}
+		
 		if(e.getSource().equals(vista.mnuiSalir)) {
 			System.exit(0);
 		}
 	}
-
+	}
 }
